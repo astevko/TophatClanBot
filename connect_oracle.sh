f12cdc49046dc0f413dc9ddeba3187f40f476dd7
astevko <andy.stevko@gmail.com>
2026-01-03 18:43:12 -0800

    connectin oracle

--- connect_oracle.sh
+++ connect_oracle.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Connect to Oracle Autonomous Database via SQLcl
 
-# Load credentials from .env file
+# Load credentials from .env file (handles .env without export statements)
+set -a
 source /home/opc/TophatClanBot/.env
+set +a
 
-# Create a login script that sets up the connection
 export TNS_ADMIN=/home/opc/TophatClanBot/wallet_config
 
-sql ${ORACLE_ADMIN_USE:R}/${ORACLE_ADMIN_PASSWORD}@perrydatabase_high
-
+sql ${ORACLE_ADMIN_USER}/${ORACLE_ADMIN_PASSWORD}@perrydatabase_high
